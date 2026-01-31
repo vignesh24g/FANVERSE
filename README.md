fb88c3608bbd1d6e70fd12591cffbd4a8b5265b1
vignesh24g <vignesh24.gv@gmail.com>
2026-01-31 10:30:15 +0000

    images added for isl,pkl, few zoom out enhancements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FANVERSE
 #IPL Theme sd map website
+#image tools https://imagestool.com/split-images, 
+#used https://bgeraser.com/
--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const map = L.map('map').setView([20.5937, 78.9629], 5);
+const map = L.map('map', {
+  minZoom: 5,
+  maxBounds: [
+    [-90, -180],
+    [90, 180]
+  ],
+  maxBoundsViscosity: 1.0
+}).setView([20.5937, 78.9629], 5);
 
 // Tile themes
 const themes = {
@@ -35,19 +42,19 @@ const leagues = {
     { name:"KKR", stadium:"Eden Gardens", coords:[22.5626,88.3639], logo:"kkr.png", state:"West Bengal", color:"purple" },
     { name:"DC", stadium:"Arun Jaitley Stadium", coords:[28.6377,77.2432], logo:"dc.png", state:"Delhi", color:"darkblue" },
     { name:"SRH", stadium:"Rajiv Gandhi Intl. Stadium", coords:[17.4065,78.5505], logo:"srh.png", state:"Telangana", color:"orange" },
-    { name:"RR", stadium:"Sawai Mansingh Stadium", coords:[26.9124,75.8080], logo:"rr.png", state:"Rajasthan", color:"pink" },
+    { name:"RR", stadium:"Sawai Mansingh Stadium", coords:[26.9124,75.8080], logo:"rr.png", state:"Rajasthan", color:"deeppink" },
     { name:"PBKS", stadium:"IS Bindra Stadium", coords:[30.7056,76.7340], logo:"pbks.png", state:"Punjab", color:"crimson" },
     { name:"GT", stadium:"Narendra Modi Stadium", coords:[23.0794,72.6167], logo:"gt.png", state:"Gujarat", color:"teal" },
-    { name:"LSG", stadium:"Ekana Stadium", coords:[26.8467,80.9462], logo:"lsg.png", state:"Uttar Pradesh", color:"green" }
+    { name:"LSG", stadium:"Ekana Stadium", coords:[26.8467,80.9462], logo:"lsg.png", state:"Uttar Pradesh", color:"palegreen" }
   ],
   pkl: [
     { name:"Patna Pirates", stadium:"Patliputra Sports Complex", coords:[25.6110,85.1440], logo:"patna.png", state:"Bihar", color:"green" },
     { name:"Bengaluru Bulls", stadium:"Kanteerava Indoor Stadium", coords:[12.9716,77.5946], logo:"bulls.png", state:"Karnataka", color:"red" },
-    { name:"Dabang Delhi KC", stadium:"Thyagaraj Indoor Stadium", coords:[28.5721,77.2432], logo:"delhi.png", state:"Delhi", color:"blue" },
-    { name:"Jaipur Pink Panthers", stadium:"SMS Indoor Stadium", coords:[26.9124,75.7873], logo:"jaipur.png", state:"Rajasthan", color:"pink" },
+    { name:"Dabang Delhi KC", stadium:"Thyagaraj Indoor Stadium", coords:[28.5721,77.2432], logo:"delhi.png", state:"Delhi", color:"red" },
+    { name:"Jaipur Pink Panthers", stadium:"SMS Indoor Stadium", coords:[26.9124,75.7873], logo:"jaipur.png", state:"Rajasthan", color:"deeppink" },
     { name:"U Mumba", stadium:"NSCI Dome", coords:[18.9388,72.8258], logo:"umumba.png", state:"Maharashtra", color:"orange" },
     { name:"Telugu Titans", stadium:"Gachibowli Indoor Stadium", coords:[17.4400,78.3489], logo:"titans.png", state:"Telangana", color:"yellow" },
-    { name:"Bengal Warriors", stadium:"Netaji Indoor Stadium", coords:[22.5626,88.3639], logo:"bengal.png", state:"West Bengal", color:"blue" },
+    { name:"Bengal Warriors", stadium:"Netaji Indoor Stadium", coords:[22.5626,88.3639], logo:"bengal.png", state:"West Bengal", color:"lightskyblue" },
     { name:"Puneri Paltan", stadium:"Balewadi Stadium", coords:[18.6715,73.7700], logo:"puneri.png", state:"Maharashtra", color:"orange" },
     { name:"Tamil Thalaivas", stadium:"SDAT Indoor Stadium", coords:[13.0636,80.2785], logo:"thalaivas.png", state:"Tamil Nadu", color:"yellow" },
     { name:"UP Yoddhas", stadium:"Babu Banarasi Das Indoor Stadium", coords:[26.8467,80.9462], logo:"upyoddha.png", state:"Uttar Pradesh", color:"blue" },
@@ -55,18 +62,18 @@ const leagues = {
     { name:"Gujarat Giants", stadium:"EKA Arena", coords:[23.0225,72.5714], logo:"giants.png", state:"Gujarat", color:"brown" }
   ],
   isl: [
-    { name:"Bengaluru FC", stadium:"Sree Kanteerava Stadium", coords:[12.9716,77.5946], logo:"bengaluru.png", state:"Karnataka", color:"blue" },
-    { name:"Chennaiyin FC", stadium:"Jawaharlal Nehru Stadium", coords:[13.0636,80.2785], logo:"chennaiyin.png", state:"Tamil Nadu", color:"navy" },
-    { name:"East Bengal FC", stadium:"Salt Lake Stadium", coords:[22.5726,88.3639], logo:"eastbengal.png", state:"West Bengal", color:"red" },
-    { name:"FC Goa", stadium:"Jawaharlal Nehru Stadium", coords:[15.4909,73.8278], logo:"goa.png", state:"Goa", color:"orange" },
-    { name:"Hyderabad FC", stadium:"GMC Balayogi Stadium", coords:[17.4065,78.5505], logo:"hyderabad.png", state:"Telangana", color:"yellow" },
-    { name:"Jamshedpur FC", stadium:"JRD Tata Sports Complex", coords:[22.8000,86.2000], logo:"jamshedpur.png", state:"Jharkhand", color:"red" },
-    { name:"Kerala Blasters FC", stadium:"Jawaharlal Nehru Stadium", coords:[9.9496,76.2590], logo:"kerala.png", state:"Kerala", color:"yellow" },
-    { name:"Mohun Bagan SG", stadium:"Salt Lake Stadium", coords:[22.5726,88.3639], logo:"mohun.png", state:"West Bengal", color:"green" },
-    { name:"Mumbai City FC", stadium:"Mumbai Football Arena", coords:[19.0760,72.8777], logo:"mumbai.png", state:"Maharashtra", color:"skyblue" },
-    { name:"NorthEast United FC", stadium:"Indira Gandhi Athletic Stadium", coords:[25.5788,91.8933], logo:"northeast.png", state:"Assam", color:"maroon" },
-    { name:"Odisha FC", stadium:"Kalinga Stadium", coords:[20.2961,85.8245], logo:"odisha.png", state:"Odisha", color:"purple" },
-    { name:"Punjab FC", stadium:"Jawaharlal Nehru Stadium", coords:[30.7333,76.7794], logo:"punjab.png", state:"Punjab", color:"crimson" }
+    { name:"Bengaluru FC", stadium:"Sree Kanteerava Stadium", coords:[12.9716,77.5946], logo:"bfc.png", state:"Karnataka", color:"blue" },
+    { name:"Chennaiyin FC", stadium:"Jawaharlal Nehru Stadium", coords:[13.0636,80.2785], logo:"cfc.png", state:"Tamil Nadu", color:"navy" },
+    { name:"East Bengal FC", stadium:"Salt Lake Stadium", coords:[22.5726,88.3639], logo:"ebfc.png", state:"West Bengal", color:"red" },
+    { name:"FC Goa", stadium:"Jawaharlal Nehru Stadium", coords:[15.4909,73.8278], logo:"fcg.png", state:"Goa", color:"darkorange" },
+    { name:"Hyderabad FC", stadium:"GMC Balayogi Stadium", coords:[17.4065,78.5505], logo:"hfc.png", state:"Telangana", color:"yellow" },
+    { name:"Jamshedpur FC", stadium:"JRD Tata Sports Complex", coords:[22.8000,86.2000], logo:"jfc.png", state:"Jharkhand", color:"maroon" },
+    { name:"Kerala Blasters FC", stadium:"Jawaharlal Nehru Stadium", coords:[9.9496,76.2590], logo:"kbfc.png", state:"Kerala", color:"yellow" },
+    { name:"Mohun Bagan SG", stadium:"Salt Lake Stadium", coords:[22.5726,88.3639], logo:"mbsg.png", state:"West Bengal", color:"green" },
+    { name:"Mumbai City FC", stadium:"Mumbai Football Arena", coords:[19.0760,72.8777], logo:"mcfc.png", state:"Maharashtra", color:"skyblue" },
+    { name:"NorthEast United FC", stadium:"Indira Gandhi Athletic Stadium", coords:[25.5788,91.8933], logo:"neufc.png", state:"Assam", color:"red" },
+    { name:"Odisha FC", stadium:"Kalinga Stadium", coords:[20.2961,85.8245], logo:"ofc.png", state:"Odisha", color:"purple" },
+    { name:"Punjab FC", stadium:"Jawaharlal Nehru Stadium", coords:[30.7333,76.7794], logo:"pfc.png", state:"Punjab", color:"crimson" }
   ]
 };
 
